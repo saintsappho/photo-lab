57c0c1365c3c6daec368bc3a5ac664de8f8a6b70
saintsappho <robinrosefleur@gmail.com>
2024-01-10 18:17:36 -0700

    css changes and classnames

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -5,12 +5,14 @@ const PhotoListItem = (props) => {
   const {imageSource, username, profile, id, location } = props.sampleDataForPhotoListItem
   const {city, country} = location
   return (
-    <article className="photo-list-item" key={"photo" + id}>
-      <img src={imageSource} alt={`Photo ${id}: Location ${city}, ${country}`} />
-      <div className="user-info">
-        <h2>{username}</h2>
-        <h3>{city}, {country}</h3>
-        <img key={username + id} src={profile} alt={`${username}'s profile`} className="profile-image" />
+    <article className="photo-list__item" key={"photo" + id}>
+      <img className="photo-list__image" src={imageSource} alt={`Photo ${id}: Location ${city}, ${country}`} />
+      <div className="photo-list__user-info">
+        <div className="photo-list__user-details">
+          <h2 className="">{username}</h2>
+          <h2 className="photo-list__user-location"> {city}, {country}</h2>
+        </div>
+        <img className="photo-list__user-profile" key={username + id} src={profile} alt={`${username}'s profile`} />
       </div>
     </article>
   )
