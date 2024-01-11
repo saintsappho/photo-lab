17922ef16b1d314f84de4beb0501ee577fd2f347
saintsappho <robinrosefleur@gmail.com>
2024-01-10 17:58:52 -0700

    display first photolistitem

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -1,21 +1,18 @@
 import React from "react";
-
 import "../styles/PhotoListItem.scss";
 
-
-const sampleDataForPhotoListItem = {
-  id: "1",
-  location: {
-    city: "Montreal",
-    country: "Canada",
-  },
-  imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
-  username: "Joe Example",
-  profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
-};
-
-const PhotoListItem = () => {
-  /* Insert React */
+const PhotoListItem = (props) => {
+  const {imageSource, username, profile, id } = props.sampleDataForPhotoListItem
+  
+  return (
+    <article className="photo-list-item" key={"photo" + id}>
+      <img src={imageSource} alt={`Photo ${id}`} />
+      <div className="user-info">
+        <h2>{username}</h2>
+        <img key={username + id} src={profile} alt={`${username}'s profile`} className="profile-image" />
+      </div>
+    </article>
+  )
 };
 
 export default PhotoListItem;
