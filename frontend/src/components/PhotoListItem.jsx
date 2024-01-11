abe660ee161cc7541f034e57c262a1c8f36a7a56
saintsappho <robinrosefleur@gmail.com>
2024-01-10 18:02:40 -0700

    display location and add to alt

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -2,13 +2,14 @@ import React from "react";
 import "../styles/PhotoListItem.scss";
 
 const PhotoListItem = (props) => {
-  const {imageSource, username, profile, id } = props.sampleDataForPhotoListItem
-  
+  const {imageSource, username, profile, id, location } = props.sampleDataForPhotoListItem
+  const {city, country} = location
   return (
     <article className="photo-list-item" key={"photo" + id}>
-      <img src={imageSource} alt={`Photo ${id}`} />
+      <img src={imageSource} alt={`Photo ${id}: Location ${city}, ${country}`} />
       <div className="user-info">
         <h2>{username}</h2>
+        <h3>{city}, {country}</h3>
         <img key={username + id} src={profile} alt={`${username}'s profile`} className="profile-image" />
       </div>
     </article>
